5a065387169de64d63b0b8c58f2231585559036f
bprigent <hello@bprigent.com>
2023-05-25 20:07:18 -0400

    added js code to get temp from api, then added it to HTML

--- temp.js
+++ temp.js
@@ -9,37 +9,47 @@ const countParam = '&cnt=5'
 const url = `${mainApiUrl}${locationParam}${countParam}${keyParam}${unitParam}`
 
 
+//create weatherData objetc
+let weatherData;
 
-// fetch weather data from API with async function
-async function getWeather() {
+// fetch weather data from API with async function then add it to object
+async function fetchWeatherData() {
 
     try {
         const response = await fetch(url);
-
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-
-        const weatherData = await response.json();
-        //console.log(weatherData.list);
-        return weatherData.list;
+        const data = await response.json();
+        weatherData = data;
 
     } catch (error) {
         console.error(`Failed to fetch the weather data: ${error}`);
     }
+
 };
 
 
 
-// Store promise value inside an object
-// create object
-let todayWeather = {};
-// store return of promise in the obj above
-async function storeDataInObj() {
-    todayWeather.data = await getWeather();
-};
-//call the function to store data in object
-storeDataInObj();
+//use weather data to add to HTML
+function useWeatherData() {
+
+    // Create temeratures
+    let temp_today =  weatherData.list[0].main.temp;
+    let temp_plus1 =  weatherData.list[1].main.temp;
+    let temp_plus2 =  weatherData.list[2].main.temp;
+    let temp_plus3 =  weatherData.list[3].main.temp;
+    let temp_plus4 =  weatherData.list[4].main.temp;
+
+    // add temeratures to html
+    document.getElementById("temp_today").innerText = `${temp_today} °C`;
+    document.getElementById("temp_plus1").innerText = `${temp_plus1} °C`;
+    document.getElementById("temp_plus2").innerText = `${temp_plus2} °C`;
+    document.getElementById("temp_plus3").innerText = `${temp_plus3} °C`;
+    document.getElementById("temp_plus4").innerText = `${temp_plus4} °C`;
+    
+}
 
 
-
+// get the data from API and the use that data inside 'useWeatherData'
+fetchWeatherData().then(useWeatherData);
